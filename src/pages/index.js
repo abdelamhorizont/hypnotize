e2ea953089bc0d4bf8d20809c9de83b25c7bcd13
abdelammeer <abdelhamid.ameur@hotmail.de>
2023-02-17 15:47:33 +0100

    fast feertig

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,6 +32,7 @@ const IndexPage = () => {
 
   const [menuOpen, setmenuOpen] = useState(false)
   const [imprintOpen, setimprintOpen] = useState(false)
+  const [privacyOpen, setprivacyOpen] = useState(false)
 
   const collapse = menuOpen ? 'collapse' : 'not-collapsed'
   const nomargin = { margin: menuOpen && 0.5 + 'rem' }
@@ -87,15 +88,15 @@ const IndexPage = () => {
 
 
       {mobile &&
-      <div className="mobile-lang">
-        <div className="lang">
-          <li className="active">de</li>
-          <li>en</li>
+        <div className="mobile-lang">
+          <div className="lang">
+            <li className="active">de</li>
+            <li>en</li>
+          </div>
         </div>
-      </div>
       }
 
-      <motion.div style={{ margin: menuOpen && (mobile? '5' : "14") + 'rem 0.5rem 0.5rem 0.5rem' }} id="services"
+      <motion.div style={{ margin: menuOpen && (mobile ? '5' : "14") + 'rem 0.5rem 0.5rem 0.5rem' }} id="services"
         initial={{ opacity: "0" }}
         animate={{ opacity: "1" }}
         transition={{ duration: 0.5, delay: 1.1 }}
@@ -193,7 +194,6 @@ const IndexPage = () => {
           }}
             href="#impressum">
             <h4>Impressum</h4></a>
-          {/* <div className={textCollapse}> */}
           <motion.div
             style={{ overflow: 'hidden' }}
             initial={{ maxHeight: 0 }}
@@ -225,13 +225,49 @@ const IndexPage = () => {
               </p>
             </div>
           </motion.div>
-          {/* </div> */}
         </div>
 
         <div id="datenschutz">
-          <h4>
-            <Link to="/privacy-policy">Datenschutz</Link>
-          </h4>
+          <a onClick={() => {
+            setmenuOpen(false)
+            setprivacyOpen(!privacyOpen)
+            if (menuOpen === true) {
+              setprivacyOpen(true)
+            }
+          }}
+            href="#datenschutz">
+            <h4>Datenschutz</h4></a>
+          <motion.div
+            style={{ overflow: 'hidden' }}
+            initial={{ maxHeight: 0 }}
+            variants={collapseVariants}
+            animate={menuOpen ? 'close' : privacyOpen ? 'open' : 'close'}
+            transition={{ duration: 0.5 }}
+          >
+            <div className='privacy'>
+              <p>
+                Et fugiat tempor eu pariatur non incididunt velit adipisicing sit fugiat eu. Aute pariatur ea tempor adipisicing. Cillum sit culpa id irure irure ad ullamco. Ad nulla occaecat voluptate labore in qui. Sint aliquip dolor in deserunt qui commodo nulla consequat veniam ad non id eiusmod. Ex tempor aute dolore nisi duis eu minim do sunt officia fugiat fugiat deserunt in.
+
+                Ea consectetur aliqua deserunt id Lorem. Reprehenderit do voluptate veniam nostrud ex deserunt culpa consectetur proident. Commodo officia consequat occaecat ullamco dolore nisi in voluptate ipsum magna sunt et.
+
+                Ut adipisicing elit incididunt qui non commodo nostrud. Dolor consectetur ullamco sint sint ad consequat sit ea deserunt nisi. Ad irure id laborum eu duis ea id aute occaecat mollit proident consequat ut. Ullamco culpa fugiat consequat velit proident. In Lorem eu consectetur aliquip cillum culpa proident. Excepteur excepteur cupidatat ea minim ullamco minim dolor sunt Lorem elit.
+
+                Ullamco amet magna sint anim. Excepteur proident do aliquip sint nulla aliquip velit fugiat commodo ut. Ea ad dolor consectetur deserunt reprehenderit anim culpa qui est fugiat. Aliquip do commodo consequat sunt nostrud non fugiat sint velit nisi duis dolore. In Lorem nulla pariatur tempor duis esse quis in. Reprehenderit fugiat mollit minim laborum cupidatat. Reprehenderit id do voluptate cillum consequat pariatur do id veniam eiusmod duis cupidatat reprehenderit.
+
+                Ut et Lorem ut ut id minim cillum officia pariatur occaecat id reprehenderit cupidatat proident. Quis officia ea sint ea laboris laboris esse ad do ad. Dolor cupidatat voluptate minim minim nisi culpa id in eiusmod cupidatat ex sint dolore sunt. Excepteur deserunt deserunt reprehenderit velit ipsum laboris eiusmod non aute.
+
+                Adipisicing veniam sint fugiat voluptate. Esse ipsum laborum aliqua adipisicing sint sit incididunt ullamco. Est esse non voluptate non tempor ut esse exercitation eiusmod aute eiusmod. Eiusmod pariatur sint minim irure esse ut exercitation non. Velit sit do consectetur ex consequat. Dolore nisi velit ad non anim veniam ut elit occaecat sit exercitation culpa.
+
+                Sunt pariatur cillum cupidatat ipsum tempor dolore et adipisicing in. Consectetur consequat velit pariatur sit proident eu adipisicing consectetur ad veniam nulla laboris consequat Lorem. Eiusmod mollit amet amet anim proident voluptate aliquip cupidatat anim. Anim incididunt culpa id est nostrud in aliquip aliqua ea laborum commodo nulla aliquip. Id velit commodo pariatur est quis adipisicing ullamco. Sit eu enim eu dolore. Ex aute esse ea proident sint.
+
+                Magna nostrud consectetur consequat nulla consequat ex. Aliqua laborum exercitation reprehenderit nisi ea. Enim aliqua dolor in sit duis veniam duis cupidatat esse exercitation irure. Culpa ex est in proident in id qui do officia est veniam eiusmod anim. Cillum aliquip sit consequat aliquip cupidatat ullamco occaecat labore.
+
+                Qui et exercitation quis nulla dolor ex esse veniam labore quis adipisicing quis. Aute fugiat ut consequat incididunt pariatur aute commodo culpa minim nisi id. Ipsum nisi nisi sint duis. Tempor est ad ea mollit et nostrud amet ex laborum tempor occaecat dolore nisi et. Labore cillum in consequat sint anim pariatur esse ut eu.
+
+                Lorem in consequat labore excepteur incididunt elit officia dolor sunt laboris magna. Proident non non labore in do enim nisi. Esse pariatur deserunt nisi id cupidatat cupidatat adipisicing. Amet laborum ad excepteur velit aliquip anim nulla adipisicing duis elit aliqua ad Lorem. Sunt incididunt ut tempor occaecat. Ea minim Lorem quis ut aliqua ad aliqua culpa esse commodo in est. Minim ea occaecat ullamco ut nostrud nulla incididunt.
+              </p>
+            </div>
+          </motion.div>
         </div>
       </div>
     </div>
